e5f0686c05a3716c01b40e020f3d5b1d78f9c135
Ben Edivri <eddyjamin@gmail.com>
2022-06-22 06:22:07 +0100

    Update profile.dart

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -9,7 +9,7 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.teal,
       ),
       body: SingleChildScrollView(
         child: Stack(
